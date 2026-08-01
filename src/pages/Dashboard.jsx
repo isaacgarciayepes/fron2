d59b648df078c546204b042269f246884bb0562d
damongu <dmonsalveal@cesde.net>
2026-08-01 16:58:03 -0500

    first commit

---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+    <div className="m-4 flex items-center justify-center">
+
+<h1 className="text-4xl font-bold text-blue-500"> 
+      Hola mundo
+      
+</h1>
+     </div>
+    </>
+  )
+}
+
+export default App
